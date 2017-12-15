fd66b4d0dcf2462d8bf5fcb235227a39c95869d6
Paul <pvdstel@users.noreply.github.com>
2017-12-15 01:36:19 +0100

    Add project files

---
+++ sudo/CommandEscaper.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace sudo
+{
+    /// <summary>
+    /// Escapes commands and arguments.
+    /// 
+    /// Modified from http://csharptest.net/529/how-to-correctly-escape-command-line-arguments-in-c/index.html.
+    /// </summary>
+    public class CommandEscaper
+    {
+        /// <summary>
+        /// Contains characters that cannot be escaped.
+        /// </summary>
+        private static readonly Regex invalid = new Regex("[\x00\x0a\x0d]");
+        /// <summary>
+        /// Contains whitespace or two quote characters.
+        /// </summary>
+        private static readonly Regex needsQuotes = new Regex(@"\s|""");
+        /// <summary>
+        /// Contains one or more '\' followed with a quote or end of string.
+        /// </summary>
+        private static readonly Regex escapeQuotes = new Regex(@"(\\*)(""|$)");
+
+        /// <summary>
+        /// Quotes arguments that contain whitespace, or begin with a quote and returns a single
+        /// argument string for use with Process.Start().
+        /// </summary>
+        /// <param name="argument">The argument to escape. May not contain null, '\0', '\r', or '\n'.</param>
+        /// <returns>A <see cref="string"/> containing the escaped argument.</returns>
+        /// <exception cref="ArgumentNullException">Raised the argument is null.</exception>
+        /// <exception cref="InvalidOperationException">Raised if the argument contains '\0', '\r', or '\n'.</exception>
+        public string Escape(string argument)
+        {
+            if (argument == null)
+            {
+                throw new ArgumentNullException(nameof(argument));
+            }
+
+            if (IsInvalid(argument))
+            {
+                throw new InvalidOperationException("This argument contains invalid characters.");
+            }
+
+            StringBuilder escaped = new StringBuilder();
+            if (argument == string.Empty)
+            {
+                escaped.Append("\"\"");
+            }
+            else if (!NeedsQuotes(argument))
+            {
+                escaped.Append(argument);
+            }
+            else
+            {
+                escaped.Append('"');
+                escaped.Append(EscapeQuotes(argument));
+                escaped.Append('"');
+            }
+            return escaped.ToString();
+        }
+
+        /// <summary>
+        /// Checks if the given argument is invalid.
+        /// </summary>
+        /// <param name="argument">The argument to check.</param>
+        /// <returns>A <see cref="bool"/> indicating whether the argument is valid.</returns>
+        private bool IsInvalid(string argument) => invalid.IsMatch(argument);
+
+        /// <summary>
+        /// Checks if the given argument needs quotes.
+        /// </summary>
+        /// <param name="argument">The argument to check.</param>
+        /// <returns>A <see cref="bool"/> indicating whether the argument needs quotes.</returns>
+        private bool NeedsQuotes(string argument) => needsQuotes.IsMatch(argument);
+
+        /// <summary>
+        /// Escapes an argument.
+        /// </summary>
+        /// <param name="argument">The argument to escape.</param>
+        /// <returns>A <see cref="string"/> containing the escaped argument.</returns>
+        private string EscapeQuotes(string argument)
+        {
+            return escapeQuotes.Replace(argument, eval =>
+            {
+                bool hasQuotes = eval.Groups[2].Value == "\"";
+
+                string backslashes = eval.Groups[1].Value;
+                string quotes = hasQuotes ? "\\\"" : "";
+
+                return backslashes + backslashes + quotes;
+            });
+        }
+    }
+}
---
+++ sudo/Sudo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace sudo
+{
+    /// <summary>
+    /// Main class of the program.
+    /// </summary>
+    public class Sudo
+    {
+        /// <summary>
+        /// Executes the progam.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            /* In this array, the first item (at 0) will be the path to the current
+             * executable. The second item (at 1) will be the executable to be
+             * invoked. Further arguments are input to the other process.
+             */
+            string[] command = Environment.GetCommandLineArgs();
+
+            if (command.Length < 2)
+            {
+                Console.WriteLine("Please specify the command to execute.");
+                return;
+            }
+
+            string executable = command[1];
+
+            CommandEscaper commandEscaper = new CommandEscaper();
+            IEnumerable<string> escapedArguments = command.Skip(2).Select(a => commandEscaper.Escape(a));
+            string arguments = string.Join(" ", escapedArguments);
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo(executable, arguments)
+            {
+                UseShellExecute = false
+            };
+            Process.Start(processStartInfo);
+        }
+    }
+}
